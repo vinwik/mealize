2e295a910ed9fa9c03aa1200fa064e0b9e3c9302
vinwik <vinwik@hotmail.com>
2020-02-22 19:32:34 +0000

    Updates

---
+++ precache-manifest.0c453694d3b304b80662603f95a9cb01.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74001f1dd1de8a5cb839d9e40c177ba6",
+    "url": "/mealize/index.html"
+  },
+  {
+    "revision": "e5236815776c86611e50",
+    "url": "/mealize/static/css/main.2add6434.chunk.css"
+  },
+  {
+    "revision": "9244c6e42927e56a0915",
+    "url": "/mealize/static/js/2.64184ee6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/mealize/static/js/2.64184ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5236815776c86611e50",
+    "url": "/mealize/static/js/main.1e4accf8.chunk.js"
+  },
+  {
+    "revision": "764b36c6de2c3601dd99",
+    "url": "/mealize/static/js/runtime-main.4271b114.js"
+  },
+  {
+    "revision": "8ece02dc5fbc62640dc0b4e6a345e2d8",
+    "url": "/mealize/static/media/Phenomena-Regular.8ece02dc.woff"
+  },
+  {
+    "revision": "60f083e8bd35dddce744b0b658c7551b",
+    "url": "/mealize/static/media/angle-left.60f083e8.svg"
+  },
+  {
+    "revision": "af630b3864c32ef497a99fb1dfd9dddd",
+    "url": "/mealize/static/media/file-alt.af630b38.svg"
+  },
+  {
+    "revision": "27b40c8c43cb66ec62f66db8508faf16",
+    "url": "/mealize/static/media/heart.27b40c8c.svg"
+  },
+  {
+    "revision": "a14598e0f7754bc71f2e8c9a8f6cab1d",
+    "url": "/mealize/static/media/minus-square.a14598e0.svg"
+  },
+  {
+    "revision": "3703a6722e42c61c004f2d7945d44d24",
+    "url": "/mealize/static/media/plus-square.3703a672.svg"
+  },
+  {
+    "revision": "bc5ac52853e8a657b3258b7cd2dc0299",
+    "url": "/mealize/static/media/recipe-img.bc5ac528.jpg"
+  },
+  {
+    "revision": "55473c271992498166a1e0682f38d393",
+    "url": "/mealize/static/media/search.55473c27.svg"
+  },
+  {
+    "revision": "24d28d043b515c864884e7ba72117dc5",
+    "url": "/mealize/static/media/shopping-cart.24d28d04.svg"
+  },
+  {
+    "revision": "fe2100226016c7273b36a7ada271889b",
+    "url": "/mealize/static/media/times.fe210022.svg"
+  },
+  {
+    "revision": "dc1fa075a50d0cd4f7acff9e9d29d082",
+    "url": "/mealize/static/media/user.dc1fa075.svg"
+  }
+]);
